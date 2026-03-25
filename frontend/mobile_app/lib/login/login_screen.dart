1e9bc69bd5a7eb98b5f457d1168db469d717642b
Saru-Work <saruatwork@gmail.com>
2026-03-25 23:55:37 +0530

    renamed dashboard

--- frontend/mobile_app/lib/login/login_screen.dart
+++ frontend/mobile_app/lib/login/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_app/screens/home_screen.dart';
+import 'package:mobile_app/screens/student_dashboard.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
