442aa233def1f8db13a81f971b70e77e13aae4a0
Sahan Lochana <w.sahanlochana@gmail.com>
2026-06-21 21:09:16 +0530

    d

--- frontend/mobile_app/lib/main.dart
+++ frontend/mobile_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'features/auth/screens/login_screen.dart';
 import 'screens/student_dashboard.dart';
 import 'screens/lecturer/lecturer_dashboard.dart';
@@ -6,21 +7,28 @@ import 'screens/campus_map_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await dotenv.load(fileName: ".env");
+  
+  
+  await dotenv.load(fileName: ".env"); 
 
   runApp(const HallSyncApp());
 }
+
 
 class HallSyncApp extends StatelessWidget {
   const HallSyncApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "HallSync",
-      home: const LoginScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/student-dashboard': (context) => const HomeScreen(),
+        '/lecturer-dashboard': (context) => const LecturerDashboard(),
+      },
     );
   }
 }
